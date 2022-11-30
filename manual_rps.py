e2d16860f9a3f81cbd2e2fa9ad1a4325d24897c9
AdjunxLynx <117390288+AdjunxLynx@users.noreply.github.com>
2022-11-30 01:00:34 +0000

    adds get_winner function with computer and user input

--- manual_rps.py
+++ manual_rps.py
@@ -19,3 +19,26 @@ def get_user_choice():
         
 get_user_choice()
         
+
+def get_winner():
+    computer = get_computer_choice()
+    player = get_user_choice()
+    if computer == "Rock" and player == "Scissors":
+        print("You lost")
+    elif computer == "Rock" and player == "Rock":
+        print("It is a tie!")
+    elif computer == "Rock" and player == "Paper":
+        print("You won!")
+    elif computer == "Paper" and player == "Scissors":
+        print("You won!")
+    elif computer == "Paper" and player == "Rock":
+        print("You lost")
+    elif computer == "Paper" and player == "Paper":
+        print("It is a tie!")
+    elif computer == "Scissors" and player == "Scissors":
+        print("It is a tie!")
+    elif computer == "Scissors" and player == "Rock":
+        print("You won!")
+    else:
+        print("You lost")
+        
