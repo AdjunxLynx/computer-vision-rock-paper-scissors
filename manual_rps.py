dc44840b641ded858ce9395871de6554f92f44e4
AdjunxLynx <117390288+AdjunxLynx@users.noreply.github.com>
2022-11-30 00:54:31 +0000

    fixes grammar issues

--- manual_rps.py
+++ manual_rps.py
@@ -2,17 +2,16 @@ import random
 
 
 def get_computer_choice():
-    options = ["rock", "paper", "scissors"]
+    options = ["Rock", "Paper", "Scissors"]
     computer_choice = random.choice(options)
     return(computer_choice)
 
 get_computer_choice()
 
 def get_user_choice():
-    options = ["rock", "paper", "scissors"]
+    options = ["Rock", "Paper", "Scissors"]
     while True:
         choice = input("Enter 'rock', 'paper' or 'scissors' ")
-        choice.lower()
         if choice in options:
             return choice
         else:
