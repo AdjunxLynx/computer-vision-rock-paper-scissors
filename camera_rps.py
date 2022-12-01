be86392d1c5949154be92a211d37549564ba4108
AdjunxLynx <117390288+AdjunxLynx@users.noreply.github.com>
2022-12-01 18:46:32 +0000

    creates the logic for the game. adds a timer to wait for user input

--- camera_rps.py
+++ camera_rps.py
@@ -2,11 +2,35 @@ import cv2
 from keras.models import load_model
 import numpy as np
 import time
+import random
 
-def get_prediction():
+def get_winner(computer_choice, user_choice):
+    
+    computer = computer_choice
+    player = user_choice
+    if computer == "Rock" and player == "Scissors":
+        return "You lost"
+    elif computer == "Rock" and player == "Rock":
+        return "It is a tie!"
+    elif computer == "Rock" and player == "Paper":
+        return "You won!"
+    elif computer == "Paper" and player == "Scissors":
+        return "You won!"
+    elif computer == "Paper" and player == "Rock":
+        return "You lost"
+    elif computer == "Paper" and player == "Paper":
+        return "It is a tie!"
+    elif computer == "Scissors" and player == "Scissors":
+        return "It is a tie!"
+    elif computer == "Scissors" and player == "Rock":
+        return "You won!"
+    else:
+        return "You lost"
+
+
+def get_prediction(model, cap):
  
-    model = load_model('keras_model.h5')
-    cap = cv2.VideoCapture(0)
+    
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     before = time.time()
     while True:
@@ -18,18 +42,69 @@ def get_prediction():
         prediction = model.predict(data)
         cv2.imshow('Rock, Papers, Scissors', frame)
         # Press q to close the window
-        print(prediction)
-        print(np.argmax(prediction))
         if time.time() - before >= 3:
             options = ["Rock", "Paper", "Scissors", "Nothing"]
-            index = np.argmax(prediction, axis = 1)
-            print(index)
-            break
+            index = np.argmax(prediction)
+            index.astype(int)
+
+            print(str(options[index]) + " Predicted")
+            return (options[index])
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+def get_computer_choice():
+    options = ["Rock", "Paper", "Scissors"]
+    computer_choice = random.choice(options)
+    return(computer_choice)
 
-
+def play():
+    model = load_model('keras_model.h5')
+    cap = cv2.VideoCapture(0)
+    rounds_played = 0
+    computer_wins=0
+    user_wins=0
+    options = ["Rock", "Paper", "Scissors", "Nothing"]
     
-get_prediction()
+    while True:
+        
+        computer_choice = get_computer_choice()
+        while True:
+            user_choice = get_prediction(model, cap)
+            print("here")
+            print(user_choice)
+            if user_choice != "Nothing":
+                break
+            else:
+                pass
+                
+            
+        winner = get_winner(computer_choice, user_choice)
+        if winner == "You lost":
+            rounds_played += 1
+            computer_wins += 1
+            waste_time()
+        elif winner == "You won!":
+            rounds_played += 1
+            user_wins += 1
+            waste_time()
+        else:
+            rounds_played += 1
+            
+        
+        if computer_wins == 3:
+            print("Computer won best of 3")
+            break
+        elif user_wins == 3:
+            print("You won best of 3")
+            break
+        else:
+            print("5 Games played")
+
+def waste_time():
+    before = time.time()
+    while True:
+        if time.time() - before >= 3:
+            break
+
+play()
